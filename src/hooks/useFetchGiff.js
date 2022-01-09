2fe03712220b6c51370e6a125e9f67fd91bfb43a
Leandro <leandroquiroga9514@gmail.com>
2022-01-08 22:12:30 -0300

    Custom Hooks

---
+++ src/hooks/useFetchGiff.js
@@ -0,0 +1,25 @@
+
+import { useState, useEffect } from 'react';
+import { fetchGiff } from './../helpers/getGift';
+
+
+export const useFetchGiffs = (category) => {
+    const [state, setState] = useState({
+        data: [],
+        loading: true
+    });
+
+
+    useEffect(() => {
+        fetchGiff(category)
+            .then(giff => {
+                    setState({
+                        data: giff,
+                        loading: false
+                    });
+            });
+    }, [category]);
+
+
+    return state;
+} 
