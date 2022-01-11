65ce53a9997472b9b20569327c2049bd6db4afb8
Leandro <leandroquiroga9514@gmail.com>
2022-01-11 12:59:31 -0300

    Init App

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import './../node_modules/animate.css/animate.css'
-
 import reportWebVitals from './reportWebVitals';
 import GifExpertApp from './GifExpertApp';
 
